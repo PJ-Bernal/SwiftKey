93a685a1069f9af746112ae9b039cc303ff7c3a0
Pablo Bernal <pj.bernal05@outlook.com>
2024-10-28 13:53:39 -0500

    final template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import TypeTest from './components/TypeTest'
+
+function App() {
+  return (
+    <div className="">
+      <TypeTest />
+    </div>
+  )
+}
+
+export default App
